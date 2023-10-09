398e72ac764ea07b9ebaf0251046c5783a5e8840
SalimKH5 <khadir.salim1@gmail.com>
2023-10-09 23:55:37 +0100

    first test

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import { Select } from './Select'
+import skillsData from './Data.json'; // Import the JSON data
+
+function App() {
+
+    const [selects,setSelects]=useState([]);
+    const [skills,setSkills] = useState(skillsData.skills);
+    const [isEditing, setIsEditing] = useState(false);
+    const handleSelectChange=(e,index)=>{
+      setSelects([...selects,{
+        id:index,
+        text:e.target.value
+      }])
+      const updatedSelects = skills.filter((skill) => skill !== e.target.value);
+      console.log(updatedSelects)
+      // Update the state with the new array
+      setSkills(updatedSelects);
+    }
+  return (
+    <div className="flex justify-center items-center h-full min-h-screen text-center bg-green-50">
+        <div className="mt-10 w-custom-600 h-auto flex flex-col gap-4  ">
+        <h1 className="text-4xl text-center  text-blue-800 font-bold">Select Your top 5 tech skills</h1>
+        <div className="overflow-auto flex justify-center h-[400px] bg-transparent sm:bg-white shadow-lg hover:shadow-xl transition-transform transform  border-gray-200 rounded-2xl">
+        <div className="p-5 pt-8 flex  w-full gap-4">
+            <div className=" w-5/6 flex flex-col gap-4">
+                <div className="flex flex-col items-center gap-4 py-5">
+                              <Select
+                                    selects={selects}
+                                    setSelects={setSelects}
+                              />
+                <select
+                  className='p-1 py-3 rounded-xl w-5/6 text-gray-500' 
+                  onChange={(e) => handleSelectChange(e, selects.length)}
+                  value={0}
+                >
+                  <option value="0">Add Skill</option>
+                  {skills.map((skill, index) => (
+                    <option key={index} value={skill}>
+                      {skill}
+                    </option>
+                  ))}
+                </select>
+            </div>    
+                
+            </div>
+            <div className="flex-4 w-40  flex flex-col gap-4 text-start">
+              <h3 className="text-blue-800">Suggested skills</h3>
+              <ul>
+                <li>+ HTML</li>
+                <li>+ CSS</li>
+                <li>+ Bootstrap</li>
+                <li>+ TypeScript</li>
+                <li>+ VueJs</li>
+                <li>+ Angular</li>
+                <li>+ Nodejs</li>
+              </ul>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+        
+    </div>
+   
+  )
+}
+
+export default App
---
+++ src/Select.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+// Define a functional component called Select that accepts props: selects and setSelects
+export const Select = ({ selects, setSelects }) => {
+  // Define a function called deleteItem that takes an index parameter
+  const deleteItem = (index) => {
+    // Use the filter method to create a new array without the item to be deleted
+    const updatedSelects = selects.filter((select, i) => i !== index);
+    // Update the state with the new array, effectively removing the item
+    setSelects(updatedSelects);
+  };
+
+  // Return a list of div elements based on the items in the 'selects' array
+  return (
+    selects.map((select, index) => (
+      <div key={index} className='p-4 flex items-center justify-between py-3 rounded-xl w-5/6 bg-blue-950 text-white'>
+        <p>{index}. {select.text}</p>
+        {/* Add an 'x' icon that calls the deleteItem function when clicked */}
+        <p onClick={() => deleteItem(index)} className="cursor-pointer">&#x2715;</p>
+      </div>
+    ))
+  );
+};
